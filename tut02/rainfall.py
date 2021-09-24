5b569fb5ee3a5032ba5fb9e72565870e2467327b
Sean Smith <seaniksmith@gmail.com>
2021-09-24 12:01:07 +1000

    Added list comprehension to rainfall solution

--- tut02/rainfall.py
+++ tut02/rainfall.py
@@ -4,7 +4,13 @@ import pytest
 Compute the average of only the positive elements in the list.
 '''
 def rainfall(integers):
-    pass
+    # for i in integers:
+    #     if not (isinstance(i, int) or isinstance(i, float)):
+    #         raise ValueError
+    if any(not (isinstance(num, int) or isinstance(num, float)) for num in integers):
+        raise ValueError("Make sure you only use ints or floats")
+    positives = [num for num in integers if num > 0]
+    return sum(positives) / len(positives) if len(positives) > 0 else None
 
 def test_simple():
     assert rainfall([1,2,3]) == 2
